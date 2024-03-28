c8758691ebef1f0ae8ef27d1a67c60fd8c416008
priyodip1027 <96992937+priyodip1027@users.noreply.github.com>
2024-03-28 10:39:43 +0530

    Add files via upload

---
+++ IT/Week 7/Q1/directory_app/models.py
@@ -0,0 +1,20 @@
+# directory_app/models.py
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    num_visits = models.IntegerField(default=0)
+    num_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
